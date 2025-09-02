0dfc6ea71441a934cad191f465efaa08c7525799
Ajeet-Edubuk <ajeet@edubukeseal.org>
2025-09-02 16:44:26 +0530

    Update constant.js

--- src/Context/constant.js
+++ src/Context/constant.js
@@ -5,4 +5,5 @@ import ABI from './updatedEdubukXDC.json'
 export const EdubukConAdd = process.env.REACT_APP_UpdatedContract;
 //XDC test net
 //export const EdubukConAdd = "0x7c84BB1D231AC10400E58B81C19F875D2E1F8e39";
+//Educhain-Testnet:0x2257a393afF316aCA3387Df44D67A1611c057B4c
 export const EdubukConABI = ABI.abi;
